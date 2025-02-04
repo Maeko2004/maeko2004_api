435bf95a154ef5fad74370a8043cb28c9c4687cc
Maeko2004 <meko.aristo@gmail.com>
2025-02-04 15:49:12 +0700

    commit 4

--- main.py
+++ main.py
@@ -28,18 +28,13 @@ def search_data(id:int):
     return {'result': result.to_dict(orient='records')}
 
 #menambahkan data
-@app.post("/data/{data}")
-def add_data(new_data:str):
-    new_data = new_data.split('-')
-    new_row = {'id': new_data[0],
-               'nama':new_data[1],
-               'age': new_data[2],
-               'job': new_data[3]}
-
-    new_row = pd.DataFrame(new_data)
+@app.post("/data/add")
+def add_data(new_data:dict):
+    global data
+    
+    new_row = pd.DataFrame([new_data])
     data = pd.concat([data, new_row], ignore_index=True)
 
-    return {'message': 'data is updated !'}
-
+    return {'message':data.to_dict(orient='records')}
 
 #127.0
